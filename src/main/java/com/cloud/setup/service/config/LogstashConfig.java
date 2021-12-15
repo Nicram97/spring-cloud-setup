bcb6e3b7e01dcee90d5fd721d94a86f2cdf925c4
Nicram97 <marcin952@gmail.com>
2021-12-15 14:35:05 +0100

    Added connection to logstash and sending logs to it.

---
+++ src/main/java/com/cloud/setup/service/config/LogstashConfig.java
@@ -0,0 +1,43 @@
+package com.cloud.setup.service.config;
+
+import ch.qos.logback.classic.LoggerContext;
+import com.cloud.setup.service.properties.AppProperties;
+import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.appender.LogstashTcpSocketAppender;
+import net.logstash.logback.encoder.LogstashEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class LogstashConfig {
+    private static final String LOGSTASH_APPENDER_NAME = "LOGSTASH";
+    private final AppProperties appProperties;
+
+    LogstashConfig(AppProperties appProperties) {
+        this.appProperties = appProperties;
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "logstash.LOGSTASH_HOST")
+    public LogstashTcpSocketAppender logstashAppender() {
+        String logstashUrl = String.format("%s:%s", appProperties.getLogstash().getHost(), appProperties.getLogstash().getPort());
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        LogstashTcpSocketAppender logstashTcpSocketAppender = new LogstashTcpSocketAppender();
+        logstashTcpSocketAppender.setName(LOGSTASH_APPENDER_NAME);
+        logstashTcpSocketAppender.setContext(loggerContext);
+        logstashTcpSocketAppender.addDestination(logstashUrl);
+        LogstashEncoder encoder = new LogstashEncoder();
+        encoder.setContext(loggerContext);
+        encoder.setIncludeContext(true);
+        encoder.setCustomFields("{\"appname\":\"" + "spring-cloud-setup" + "\"}");
+        encoder.start();
+        logstashTcpSocketAppender.setEncoder(encoder);
+        logstashTcpSocketAppender.start();
+        loggerContext.getLogger(Logger.ROOT_LOGGER_NAME).addAppender(logstashTcpSocketAppender);
+        return logstashTcpSocketAppender;
+    }
+}

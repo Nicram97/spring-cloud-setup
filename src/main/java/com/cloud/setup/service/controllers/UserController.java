7483d7592c057f086d777301bac408dc902e31ea
Nicram97 <marcin952@gmail.com>
2021-12-21 13:56:17 +0100

    Added open-api integration

--- src/main/java/com/cloud/setup/service/controllers/UserController.java
+++ src/main/java/com/cloud/setup/service/controllers/UserController.java
@@ -3,9 +3,15 @@ package com.cloud.setup.service.controllers;
 import com.cloud.setup.service.dto.CreateUserDto;
 import com.cloud.setup.service.entities.User;
 import com.cloud.setup.service.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpServerErrorException;
 
 @Slf4j
 @RestController("/user")
@@ -15,16 +21,40 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Get list of all created users",
+            description = "Return all user Entities saved in the database",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Users list returned form database", content = @Content(array = @ArraySchema(schema = @Schema(implementation = User.class)))),
+                    @ApiResponse(responseCode = "500", description = "Internal error, can't return users list", content = @Content(schema = @Schema(implementation = HttpServerErrorException.InternalServerError.class))),
+            }
+    )
     @GetMapping("/user")
     public Iterable<User> getAllUsers() {
         return this.userService.findAll();
     }
 
+    @Operation(
+            summary = "Get user by id",
+            description = "Return user Entity with given id value",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User with specified id returned", content = @Content(schema = @Schema(implementation = User.class))),
+                    @ApiResponse(responseCode = "404", description = "cant extract user with given id", content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class))),
+            }
+    )
     @GetMapping("/user/{id}")
     public User getUserById(@PathVariable("id") Long id) {
         return this.userService.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
+    @Operation(
+            summary = "Create user",
+            description = "Save user with provided info in database",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "User with specified id returned", content = @Content(schema = @Schema(implementation = User.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal error, user was not added to database", content = @Content(schema = @Schema(implementation = HttpServerErrorException.InternalServerError.class))),
+            }
+    )
     @PostMapping("/user")
     public User createUser(@RequestBody CreateUserDto createUserDto) {
         User user = new User();

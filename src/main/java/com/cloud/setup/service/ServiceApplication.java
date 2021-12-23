cc7a27efa96ae4fabc49db6c8c9a4d645d12a344
Nicram97 <marcin952@gmail.com>
2021-12-23 12:04:41 +0100

    Added cache mechanism

--- src/main/java/com/cloud/setup/service/ServiceApplication.java
+++ src/main/java/com/cloud/setup/service/ServiceApplication.java
@@ -3,9 +3,11 @@ package com.cloud.setup.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableCaching
 public class ServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/cloud/setup/service/controllers/UserController.java
+++ src/main/java/com/cloud/setup/service/controllers/UserController.java
@@ -9,6 +9,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpServerErrorException;
@@ -29,6 +32,7 @@ public class UserController {
                     @ApiResponse(responseCode = "500", description = "Internal error, can't return users list", content = @Content(schema = @Schema(implementation = HttpServerErrorException.InternalServerError.class))),
             }
     )
+    @Cacheable("users")
     @GetMapping("/user")
     public Iterable<User> getAllUsers() {
         return this.userService.findAll();
@@ -55,6 +59,7 @@ public class UserController {
                     @ApiResponse(responseCode = "500", description = "Internal error, user was not added to database", content = @Content(schema = @Schema(implementation = HttpServerErrorException.InternalServerError.class))),
             }
     )
+    @CacheEvict(value = "users", allEntries = true)
     @PostMapping("/user")
     public User createUser(@RequestBody CreateUserDto createUserDto) {
         User user = new User();
